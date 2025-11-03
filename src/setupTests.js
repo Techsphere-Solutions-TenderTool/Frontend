09d2870e14391fb9f2fa91fef7cba9c16051affb
11Hamza <ST10268917@vcconnect.edu.za>
2025-11-03 10:05:57 +0200

    configuring pipeline

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,10 @@
-import '@testing-library/jest-dom';
+// src/setupTests.js
+
+import { afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+
+// Clean up the DOM after every test
+afterEach(() => {
+  cleanup();
+});
